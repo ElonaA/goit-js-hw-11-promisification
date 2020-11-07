04a99131ccdaddea132acf59cb2be6d2a3c3473d
Elona Atroshchenko <ell.atroshchenko@gmail.com>
2020-11-08 00:01:40 +0200

    Добавила решение задачи

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/task-01';
+import './js/task-02';
+import './js/task-03';
+
+
+
---
+++ src/js/task-01.js
@@ -0,0 +1,12 @@
+const delay = ms => {
+  return new Promise(resolve => {
+    setTimeout(resolve(ms), ms);
+  });
+}
+
+const logger = time => console.log(`Resolved after ${time}ms 🚀`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
